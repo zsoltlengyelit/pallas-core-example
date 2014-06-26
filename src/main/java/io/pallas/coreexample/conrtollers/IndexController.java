efdb846e7b290c0891a798d59cba9d4434289389
Zsolt Lengyel <zsolt.lengyel.it@gmail.com>
2014-06-26 02:01:05 +0200

    Query param support example

--- src/main/java/io/pallas/coreexample/conrtollers/IndexController.java
+++ src/main/java/io/pallas/coreexample/conrtollers/IndexController.java
@@ -3,12 +3,20 @@ package io.pallas.coreexample.conrtollers;
 import io.pallas.core.annotations.Controller;
 import io.pallas.core.annotations.DefaultAction;
 
+import javax.inject.Inject;
+import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.QueryParam;
+
 @Controller
 public class IndexController {
 
+    @Inject
+    private HttpServletRequest request;
+
     @DefaultAction
-    public String def() {
-        return "Some template";
+    public String def(@QueryParam("name") Long name) {
+
+        return "Some template: " + name;
     }
 
     public String index() {
